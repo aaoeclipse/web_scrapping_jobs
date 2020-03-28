540c2cad79769c32dd6693935dc63fac4fb1df7e
eclipse <eclipse@pop-os.localdomain>
2020-03-28 16:14:52 -0600

    added comments

--- Card.py
+++ Card.py
@@ -3,6 +3,7 @@ import re
 class Card:
     
     def __init__(self, title, location, ammount, description):
+        ''' Initilazies the class with the given values, it also adds the minmum and maximum of the salary '''
         self.title = title
         self.location = location
         self.ammount = ammount
@@ -10,19 +11,26 @@ class Card:
         self.description = description
 
     def getMinMax(self, ammount):
+        ''' transform the ammount in integers'''
+        # if it's unknown then we set them to 0
         if ammount is 'unknown':
             self.minimum = 0
             self.maximum = 0
             return
 
+        # we separate into two by using the -, examlpe $40 - $200 is going to be [0] = $40, [1] = $200
         separated_ammount = ammount.split('-')
+        # if it didn't manage to separate it means ther is only one value ex. $15
         if len(separated_ammount) == 1:
-            self.minimum = re.sub("\D", "", separated_ammount[0])
-            self.maximum = re.sub("\D", "", separated_ammount[0])
+            self.minimum = int(re.sub("\D", "", separated_ammount[0]))
+            self.maximum = int(re.sub("\D", "", separated_ammount[0]))
             return 
 
-        self.minimum=re.sub("\D", "", separated_ammount[0])
-        self.maximum=re.sub("\D", "", separated_ammount[1])
+        # we then remove the dollar sign and every other string that is not a number
+        self.minimum=int(re.sub("\D", "", separated_ammount[0]))
+        self.maximum=int(re.sub("\D", "", separated_ammount[1]))
+
     
     def __str__(self):
+        ''' This is the standard method to return a string representation of the class '''
         return '{}\n{}\n{}\n{}\n'.format(self.title,self.location,self.ammount,self.description)
--- scrapping.py
+++ scrapping.py
@@ -2,45 +2,53 @@ import requests
 from bs4 import BeautifulSoup
 from Card import Card
 
+# list_of_cards is going to contain all the jobs which are stored as a Card class
 list_of_cards = []  
-base_page = 'https://www.simplyhired.com'
-
 page_counter = 1
 
+# the base page is always going to be the same, only the later part is going to differ (link)
+base_page = 'https://www.simplyhired.com'
+# this is the link of the first page
 link = '/search?q=marketing&l=New+Orleans%2C+LA&pn=&job=D6iraakU-GxMN-ufAR2E3a_4gB0LkhWWOmshmTSiCiAqZ8GutM9ZJw'
 
-while(link != None):
+while(True):
+    # Show current page
     print('page: {}'.format(page_counter))
     page_counter += 1
 
+    # Show current Link
     print('{}{}'.format(base_page,link))
 
+    # Get data from the link
     data = requests.get('{}{}'.format(base_page,link))
     soup = BeautifulSoup(data.text, 'html.parser')
 
+    # Get all articles (cards) in the page
     for article in soup.findAll('article'):
+        # Get info (title, location, ammount, and description) for each one of them
         title = article.find('div', { 'class': 'jobposting-title-container'}).text
         location = article.find('span', { 'class': 'JobPosting-labelWithIcon jobposting-company'}).text
         ammount = article.find('span', { 'class': 'jobposting-salary'})
-
+        description = article.find('p', { 'class': 'jobposting-snippet'}).text
+        # sometimes there is no salary
         if ammount is None:
             ammount = "unknown"
         else:
             ammount = ammount.text
-
-        description = article.find('p', { 'class': 'jobposting-snippet'}).text
+        
+        # add a new Card to the list of cards with the current values
         curr_card = Card(title, location, ammount, description)
         list_of_cards.append(curr_card)
-    
+    # get the new link with the '->' that's in the page
     link = soup.find('a', { 'class': 'next-pagination'})
+    # if it doesn't have one, it means we have reached the end page so we break out of the while loop
     if link is None:
         break
     link = link['href']
 
+# Now we record each statement on the file named jobs.txt
 f = open("jobs.txt","w")
-
 for card in list_of_cards:
     f.write(card.__str__())
     f.write('\n')
-
 f.close()
